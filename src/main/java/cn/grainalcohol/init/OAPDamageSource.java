556475fec95f54165a813a9134d3d1696f7f1ed9
GRAINALCOHOL <132149646+GRAINALCOHOL@users.noreply.github.com>
2025-05-30 05:40:40 +0800

    缺氧伤害不再会被护甲值减免，加入了真实伤害类型

--- src/main/java/cn/grainalcohol/init/OAPDamageSource.java
+++ src/main/java/cn/grainalcohol/init/OAPDamageSource.java
@@ -4,11 +4,15 @@ import cn.grainalcohol.OAPMod;
 import net.minecraft.entity.damage.DamageType;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
 
 public class OAPDamageSource {
     public static final RegistryKey<DamageType> CHOKING =
             RegistryKey.of(RegistryKeys.DAMAGE_TYPE,
-                    new Identifier(OAPMod.MOD_ID, "choking")
+                    OAPMod.id("choking")
+            );
+
+    public static final RegistryKey<DamageType> TRUE_DAMAGE =
+            RegistryKey.of(RegistryKeys.DAMAGE_TYPE,
+                    OAPMod.id("true_damage")
             );
 }
