1d06ebaafb3eff9b36ea2edb4c585ccde9843151
GRAINALCOHOL <132149646+GRAINALCOHOL@users.noreply.github.com>
2025-07-07 01:27:12 +0800

    优化oap:status_effect的检查逻辑，新增require_all字段以检查是否需要持有指定的所有效果

--- src/main/java/cn/grainalcohol/condition/entity/enhanced/EnhancedStatusEffectCondition.java
+++ src/main/java/cn/grainalcohol/condition/entity/enhanced/EnhancedStatusEffectCondition.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -19,6 +20,7 @@ import java.util.function.BiFunction;
  * <br>
  * 检测实体身上的状态效果<br>
  * 这是对原版（指origins）状态效果条件的增强版本，提供更精细的控制和匹配逻辑
+ * 兼容无限时长状态效果
  *
  * <p><b>JSON字段说明:</b></p>
  * <ul>
@@ -28,7 +30,8 @@ import java.util.function.BiFunction;
  *   <li><b>max_amplifier</b> ({@code int}, 可选): 状态效果的最大效果倍率，默认为255</li>
  *   <li><b>min_duration</b> ({@code int}, 可选): 状态效果的最小时长(游戏刻)，以tick为单位，默认为20tick</li>
  *   <li><b>max_duration</b> ({@code int}, 可选): 状态效果的最大时长(游戏刻)，以tick为单位，默认为600tick</li>
- *   <li><b>check_all</b> ({@code boolean}, 可选): 设置是否需要对指定的所有状态效果进行完全匹配，默认为false</li>
+ *   <li><b>check_all</b> ({@code boolean}, 可选): 是否需要对指定的所有状态效果进行完全匹配，默认为false</li>
+ *   <li><b>require_all</b> ({@code boolean}, 可选): 是否需要持有指定的所有状态效果，默认为false</li>
  * </ul>
  */
 public class EnhancedStatusEffectCondition implements BiFunction<SerializableData.Instance, Entity, Boolean> {
@@ -39,7 +42,8 @@ public class EnhancedStatusEffectCondition implements BiFunction<SerializableDat
             .add("max_amplifier", SerializableDataTypes.INT, 255)
             .add("min_duration", SerializableDataTypes.INT, 20)
             .add("max_duration", SerializableDataTypes.INT, 600)
-            .add("check_all", SerializableDataTypes.BOOLEAN, false);
+            .add("check_all", SerializableDataTypes.BOOLEAN, false)
+            .add("require_all", SerializableDataTypes.BOOLEAN, false);
 
     @Override
     public Boolean apply(SerializableData.Instance data, Entity entity) {
@@ -47,19 +51,19 @@ public class EnhancedStatusEffectCondition implements BiFunction<SerializableDat
             return false;
         }
 
-        Set<StatusEffect> effects = new HashSet<>();
-
-        // 处理单个effect
-        if (data.isPresent("effect")) {
-            StatusEffect effect = Registries.STATUS_EFFECT.get(data.getId("effect"));
-            if (effect != null) {
-                effects.add(effect);
-            }
+        // 是否有任何状态效果，啥也没有肯定是false
+        if (livingEntity.getStatusEffects().isEmpty()) {
+            return false;
         }
 
-        // 处理多个effects
-        if (data.isPresent("effects")) {
-            List<Identifier> effectIds = data.get("effects");
+        Set<StatusEffect> effects = new HashSet<>();
+        Identifier effectId = data.getId("effect");
+        List<Identifier> effectIds = data.get("effects");
+
+        if (effectIds != null) {
+            effects.add(Registries.STATUS_EFFECT.get(effectId));
+        }
+        if (effectIds != null && !effectIds.isEmpty()) {
             effectIds.forEach(id -> {
                 StatusEffect effect = Registries.STATUS_EFFECT.get(id);
                 if (effect != null) {
@@ -68,54 +72,48 @@ public class EnhancedStatusEffectCondition implements BiFunction<SerializableDat
             });
         }
 
-        boolean checkAll = data.getBoolean("check_all");
-        boolean hasEffect;
+        boolean requireAll = data.getBoolean("require_all");
 
+        Collection<StatusEffectInstance> effectInstances;
         if (effects.isEmpty()) {
-            // 如果没有指定任何effect，则检查所有效果
-            hasEffect = !livingEntity.getActiveStatusEffects().isEmpty(); // 真没指定：（加反转）无为false，有为true
+            effectInstances = livingEntity.getStatusEffects();
         } else {
-            // 根据check_all决定是anyMatch还是allMatch
-            hasEffect = checkAll ?
-                    effects.stream().allMatch(livingEntity::hasStatusEffect) : // 所有效果都必须存在
-                    effects.stream().anyMatch(livingEntity::hasStatusEffect); // 任意一个效果存在即可
-        }
-
-        if(!hasEffect){
-            return data.getBoolean("invert");
-        }
-
-        // 检查效果强度和时间
-        StatusEffectInstance instance =
-                livingEntity.getActiveStatusEffects().values().stream()
-                .filter(e -> effects.isEmpty() // 未指定任何效果：过滤并保留所有获取到的实例
-                        || effects.contains(e.getEffectType())) // 否则（已指定）：过滤并保留包含于指定集合中的实例
-                .findFirst() // 因为只需要一个满足条件的效果实例，因此保留第一个即可
-                .orElse(null); // 玩家没有任何效果 和 玩家没有与指定效果相同的效果
-        // 如果没有过滤出任何效果，那么findFirst()将找不到任何值，则返回null
-
-        if (instance != null) {
-            int amplifier = instance.getAmplifier();
-            int duration = instance.getDuration();
-
-            if (data.isPresent("min_amplifier") && amplifier < data.getInt("min_amplifier")) {
-                hasEffect = false;
+            // 检查是否满足require_all条件
+            if (requireAll && !effects.stream().allMatch(livingEntity::hasStatusEffect)) {
+                return false;
             }
-            if (data.isPresent("max_amplifier") && amplifier > data.getInt("max_amplifier")) {
-                hasEffect = false;
-            }
-            // 无限持续时间可以通过
-            if (data.isPresent("min_duration") &&
-                    duration < data.getInt("min_duration") && !instance.isInfinite()) {
-                hasEffect = false;
-            }
-            // 无限持续时间无法通过
-            if (data.isPresent("max_duration") &&
-                    (instance.isInfinite() || duration > data.getInt("max_duration"))) {
-                hasEffect = false;
+
+            effectInstances = livingEntity.getStatusEffects().stream()
+                    .filter(e -> effects.contains(e.getEffectType()))
+                    .toList();
+            // 实体未持有指定的状态效果
+            if (effectInstances.isEmpty()) {
+                return false;
             }
         }
 
-        return hasEffect;
+        return statusEffectProcess(data, effectInstances, data.getBoolean("check_all"));
+    }
+
+    private static boolean statusEffectProcess(SerializableData.Instance data, Collection<StatusEffectInstance> instances, boolean checkAll) {
+        int minAmplifier = data.getInt("min_amplifier");
+        int maxAmplifier = data.getInt("max_amplifier");
+        int minDuration = data.getInt("min_duration");
+        int maxDuration = data.getInt("max_duration");
+
+        return checkAll ?
+        instances.stream().allMatch(instance ->
+                instance.getAmplifier() >= minAmplifier
+                        && instance.getAmplifier() <= maxAmplifier
+                        && (instance.isInfinite() || instance.getDuration() >= minDuration)
+                        && instance.getDuration() <= maxDuration
+        )
+        :
+        instances.stream().anyMatch(instance ->
+                instance.getAmplifier() >= minAmplifier
+                        && instance.getAmplifier() <= maxAmplifier
+                        && (instance.isInfinite() || instance.getDuration() >= minDuration)
+                        && instance.getDuration() <= maxDuration
+        );
     }
 }
